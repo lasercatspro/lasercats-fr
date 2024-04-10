23086db152c3431a7e7b4032d184563e1fa34859
Antoine Brique <antoine.brique@gmail.com>
2024-04-10 15:45:12 +0200

    :lipstick: Update transition for white-grid and update antoine's photo

---
+++ src/lib/members.ts
@@ -0,0 +1,51 @@
+import { Member } from "@/types/items";
+
+export const members: Member[] = [
+	{
+		name: "Nico",
+		role: "Co-gérant & Dev",
+		imageSrc: "/assets/images/portraits/nicolas.jpg",
+	},
+	{
+		name: "Thierry",
+		role: "Lead Dev",
+		imageSrc: "/assets/images/portraits/thierry.png",
+	},
+	{
+		name: "Louis",
+		role: "Co-gérant & Dev",
+		imageSrc: "/assets/images/portraits/louis.png",
+	},
+	{
+		name: "François",
+		role: "Lead Dev",
+		imageSrc: "/assets/images/portraits/francois.png",
+	},
+	{
+		name: "Damien",
+		role: "Dev",
+		imageSrc: "/assets/images/portraits/damien.png",
+	},
+	{
+		name: "Anthonin",
+		role: "Dev",
+		imageSrc: "/assets/images/portraits/anthonin.png",
+	},
+	{
+		name: "Antoine",
+		role: "Dev",
+		imageSrc: "/assets/images/portraits/antoine.png",
+	},
+];
+
+export function randomMembers(array: any[]) {
+	const randomArray: any[] = [];
+	const copiedArray = array.slice();
+
+	array.forEach(() => {
+		const randomIndex = Math.floor(Math.random() * copiedArray.length);
+		randomArray.push(copiedArray.at(randomIndex));
+		copiedArray.splice(randomIndex, 1);
+	});
+	return randomArray;
+}
