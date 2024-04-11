99e52649d9aef0a54d4de59b22c4a6fc3707d7fa
Antoine Brique <antoine.brique@gmail.com>
2024-04-11 09:58:09 +0200

    :zap: Improve lenis component

--- src/_components/smootScroll.tsx
+++ src/_components/smootScroll.tsx
@@ -1,11 +1,25 @@
 "use client";
 
 import { ReactLenis } from "@studio-freight/react-lenis";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef } from "react";
+import gsap from "gsap";
 
 function SmoothScrolling({ children }: { children: ReactNode}) {
+	const lenisRef = useRef<any>(null);
+  
+	useEffect(() => {
+		function update(time: any) {
+			if (lenisRef?.current) return lenisRef.current?.lenis?.raf(time * 1000);
+		}
+  
+		gsap.ticker.add(update);
+  
+		return () => {
+			gsap.ticker.remove(update);
+		};
+	});
 	return (
-		<ReactLenis root options={{ lerp: 0.1, duration: 1.5, syncTouch: true }}>
+		<ReactLenis ref={lenisRef} root options={{ lerp: 0.1, duration: 1.5, syncTouch: true }} autoRaf={false}>
 			{children}
 		</ReactLenis>
 	);
