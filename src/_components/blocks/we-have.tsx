de3a9218f7e04ece9c400aaabda367564c39fde6
Antoine Brique <antoine.brique@gmail.com>
2024-04-17 17:00:52 +0200

    :lipstick: Update transition for we-have block

--- src/_components/blocks/we-have.tsx
+++ src/_components/blocks/we-have.tsx
@@ -6,99 +6,187 @@ import { Transition } from "@headlessui/react";
 import useIsMobile from "@/hooks/useIsMobile";
 
 type Props = {
-	isLikeContent?: boolean
-}
+  isLikeContent?: boolean;
+};
 
 const WeHave = ({ isLikeContent = false }: Props) => {
-	const gridRef = useRef<HTMLDivElement | null>(null); 
-	const [type, setType] = useState<"exp" | "skill" | "challenges" | "vite">(isLikeContent ? "challenges": "exp");
-	const isMobile = useIsMobile({forIpad: false});
+	const gridRef = useRef<HTMLDivElement | null>(null);
+	const [type, setType] = useState<"exp" | "skill" | "challenges" | "vite">(
+		isLikeContent ? "challenges" : "exp"
+	);
+	const isMobile = useIsMobile({ forIpad: false });
 
 	useEffect(() => {
 		const handleScroll = () => {
 			if (gridRef?.current) {
 				const gridTop = gridRef?.current.offsetTop;
-				if (window.pageYOffset > (gridTop + 200) && !isLikeContent) {
+				if (window.pageYOffset > gridTop + 200 && !isLikeContent) {
 					setType("skill");
 				} else if (!isLikeContent) {
 					setType("exp");
 				}
-				if (window.pageYOffset > (gridTop + 200) && isLikeContent) {
+				if (window.pageYOffset > gridTop + 200 && isLikeContent) {
 					setType("vite");
 				} else if (isLikeContent) {
 					setType("challenges");
 				}
 			}
-			
 		};
-      
+
 		window.addEventListener("scroll", handleScroll);
-    
+
 		return () => window.removeEventListener("scroll", handleScroll);
 	}, [gridRef]);
 
-	const transition = useMemo(() => (
-		{
-			enter: "transform transition-all ease-linear duration-700",
-			enterFrom: type === "exp" || type === "challenges" ? "-translate-y-full opacity-0" : "translate-y-full opacity-0",
-			enterTo: type === "exp" || type === "challenges" ? "-translate-y-20 opacity-50" : "translate-y-20 opacity-50",
-			leave: "transform transition-all ease-linear duration-100",
-			leaveFrom: type === "exp" || type === "challenges" ? "-translate-y-20 opacity-50" : "translate-y-20 opacity-50",
-			leaveTo: type === "exp" || type === "challenges" ? "translate-y-full opacity-0" : "-translate-y-full opacity-0"
-		}
-	), [type]);
-  
+	const transition = useMemo(
+		() => ({
+			enter: "transform transition-all ease-in duration-1000",
+			enterFrom:
+        type === "exp" || type === "challenges" ? "opacity-0" : " opacity-0",
+			enterTo:
+        type === "exp" || type === "challenges" ? "opacity-70" : " opacity-70",
+			leave: "transform transition-all ease-out duration-50",
+			leaveFrom:
+				type === "exp" || type === "challenges" ? "opacity-0" : "opacity-0",
+			leaveTo:
+        type === "exp" || type === "challenges" ? "opacity-0" : "opacity-0",
+		}),
+		[type]
+	);
+
+	const subTitle = useMemo(
+		() =>
+			!isLikeContent
+				? type === "exp"
+					? "l'expérience métier"
+					: "les compétences"
+				: type === "challenges"
+					? "les challenges"
+					: "aller vite",
+		[type]
+	);
+
 	return (
-		<div ref={gridRef} className={`relative h-[175vh] ${!isMobile && "text-center"}`}>
+		<div
+			ref={gridRef}
+			className={`relative h-[175vh] ${!isMobile && "text-center"}`}
+		>
 			<WhiteGrid type={type}>
-				<div style={{background: "radial-gradient(closest-side, rgba(250,249,247,1) 50%, rgba(0,0,0,0) 100%)"}} className="mx-auto max-w-7xl py-24 px-12 lg:py-64 lg:px-32 relative">
-					<Transition
-						show={type === "exp"}
-						{...transition}
-					>
-						{!isLikeContent && <div className="flex flex-col gap-8 max-w-5xl mx-auto">
-							<h2 className="!text-3xl md:!text-5xl">Nous avons <br /><strong className="!text-3xl md:!text-5xl">{"l'expérience métier"}</strong></h2>
-							<p className="text-xs lg:text-xl">Nous sommes d’anciens CTO et artisans du web depuis loooongtemps : dans les secteurs de l’édition, des startups, des marketplaces, de l’automation, de la protection des donnés, de l’open source. Nous connaissons probablement vos problématiques et saurons trouver comment construire ou améliorer votre produit.</p>
-							<p className="font-extrabold text-sm lg:text-xl">Nous pourrons vous conseiller sur votre stratégie produit et développement logiciel global</p>
-						</div>}
-					</Transition>
-					<Transition
-						show={type === "skill"}
-						{...transition}
-					>
-						{!isLikeContent && <div className="flex flex-col gap-8 max-w-5xl mx-auto">
-							<h2 className="!text-3xl md:!text-5xl">Nous avons <br /><strong className="!text-3xl md:!text-5xl !text-custom-blue">{"les compétences"}</strong></h2>
-							<p className="text-xs lg:text-xl">Nous maîtrisons tout ce qu’il faut pour construire n’importe quelle application web ou mobile. Nous avons toutes les compétences essentielles en interne et pouvons compter sur notre galaxie de partenaires fantastiques quand il faut pousser dans une direction : conseil en scalabilité, SEO, devops, dataviz, produit, sécurité, hardware, performance, accessibilité, écologie numérique, recherche UX…</p>
-							<p className="font-extrabold text-sm lg:text-xl">Nous pouvons couvrir 100% de vos besoins pour votre projet</p>
-						</div>}
-					</Transition>
-					{/* We Like */}
-					<Transition
-						show={type === "challenges"}
-						{...transition}
-					>
-						{isLikeContent && <div className="flex flex-col gap-8 max-w-5xl mx-auto">
-							<h2 className="!text-3xl md:!text-5xl">Nous aimons <br /><strong className="!text-3xl md:!text-5xl">{"les challenges"}</strong></h2>
-							<p className="text-xs lg:text-xl">Nous adorons la stabilité des framework simples, mais aimons aussi quand les challenges sont fous et débordent du web : hardware, musique, blockchain, IA, sécurité et vie privée. Si vous projets ont besoin d’une tech inédite, c’est fort probable qu’on ait envie de faire un bout de chemin avec vous.</p>
-							<p className="font-extrabold text-sm lg:text-xl">
-							Nous pourrons réaliser votre produit, qu’elle qu’en soit, aussi fou soit-il son niveau de folie.
-							</p>
-						</div>}
-					</Transition>
-					<Transition
-						show={type === "vite"}
-						{...transition}
-					>
-						{isLikeContent && <div className="flex flex-col gap-8 max-w-5xl mx-auto">
-							<h2 className="!text-3xl md:!text-5xl">Nous aimons <br /><strong className="!text-3xl md:!text-5xl !text-custom-blue">{"aller vite"}</strong></h2>
-							<p className="text-xs lg:text-xl">
-								Nous savons aller vite pour tester un produit ou une intuition en version beta et lui faire rencontrer son marché, puis travailler plus lentement quand il commence à avoir de la traction et qu’il a besoin d’une plus grande stabilité.Pour sommes expert de 3 frameworks que nous connaissons par coeur et qui n’ont rien à prouver:
-							</p>
-							<p className="font-extrabold text-sm lg:text-xl">
-								Votre produit pourra être rapidement mis sur le marché
-							</p>
-						</div>}
-					</Transition>
+				<div
+					style={{
+						background:
+              "radial-gradient(closest-side, rgba(250,249,247,1) 50%, rgba(0,0,0,0) 100%)",
+					}}
+					className="mx-auto max-w-7xl w-screen py-24 px-12 lg:py-64 lg:px-32 relative"
+				>
+					<div className="absolute flex flex-col w-full top-24 max-w-5xl mx-auto">
+						{!isLikeContent && (
+							<h2 className="!text-3xl md:!text-5xl">
+                Nous avons <br />
+								
+							</h2>
+						)}
+						{isLikeContent && (
+							<h2 className="!text-3xl md:!text-5xl">
+                Nous aimons <br />
+							</h2>
+						)}
+						<div className="h-1/2 space-y-4">
+							{/* We have */}
+							<Transition show={type === "exp"} {...transition} className={"absolute w-full"}>
+								{!isLikeContent && (
+									<>
+										<strong
+											className="!text-3xl md:!text-5xl"
+										>
+											{subTitle}
+										</strong>
+										<p className="mt-8 text-xs lg:text-xl">
+                    Nous sommes d’anciens CTO et artisans du web depuis
+                    loooongtemps : dans les secteurs de l’édition, des startups,
+                    des marketplaces, de l’automation, de la protection des
+                    donnés, de l’open source. Nous connaissons probablement vos
+                    problématiques et saurons trouver comment construire ou
+                    améliorer votre produit.
+										</p>
+										<p className="font-extrabold text-sm lg:text-xl">
+                    Nous pourrons vous conseiller sur votre stratégie produit et
+                    développement logiciel global
+										</p>
+									</>
+								)}
+							</Transition>
+							<Transition show={type === "skill"} {...transition} className={"absolute w-full"}>
+								{!isLikeContent && (
+									<>
+										<strong
+											className="!text-3xl md:!text-5xl !text-custom-blue"
+										>
+											{subTitle}
+										</strong>
+										<p className="mt-8 text-xs lg:text-xl">
+                    Nous maîtrisons tout ce qu’il faut pour construire n’importe
+                    quelle application web ou mobile. Nous avons toutes les
+                    compétences essentielles en interne et pouvons compter sur
+                    notre galaxie de partenaires fantastiques quand il faut
+                    pousser dans une direction : conseil en scalabilité, SEO,
+                    devops, dataviz, produit, sécurité, hardware, performance,
+                    accessibilité, écologie numérique, recherche UX…
+										</p>
+										<p className="font-extrabold text-sm lg:text-xl">
+                    Nous pouvons couvrir 100% de vos besoins pour votre projet
+										</p>
+									</>
+								)}
+							</Transition>
+							{/* We Like */}
+							<Transition show={type === "challenges"} {...transition} className={"absolute w-full"}>
+								{isLikeContent && (
+									<>
+										<strong
+											className="!text-3xl md:!text-5xl"
+										>
+											{subTitle}
+										</strong>
+										<p className="mt-8 text-xs lg:text-xl">
+                    Nous adorons la stabilité des framework simples, mais aimons
+                    aussi quand les challenges sont fous et débordent du web :
+                    hardware, musique, blockchain, IA, sécurité et vie privée.
+                    Si vous projets ont besoin d’une tech inédite, c’est fort
+                    probable qu’on ait envie de faire un bout de chemin avec
+                    vous.
+										</p>
+										<p className="font-extrabold text-sm lg:text-xl">
+                    Nous pourrons réaliser votre produit, qu’elle qu’en soit,
+                    aussi fou soit-il son niveau de folie.
+										</p>
+									</>
+								)}
+							</Transition>
+							<Transition show={type === "vite"} {...transition} className={"absolute w-full"}>
+								{isLikeContent && (
+									<>
+										<strong
+											className="!text-3xl md:!text-5xl !text-custom-blue" 
+										>
+											{subTitle}
+										</strong>
+										<p className="mt-8 text-xs lg:text-xl">
+                    Nous savons aller vite pour tester un produit ou une
+                    intuition en version beta et lui faire rencontrer son
+                    marché, puis travailler plus lentement quand il commence à
+                    avoir de la traction et qu’il a besoin d’une plus grande
+                    stabilité. Nous sommes expert de 3 frameworks que nous
+                    connaissons par coeur et qui n’ont rien à prouver:
+										</p>
+										<p className="font-extrabold text-sm lg:text-xl">
+                    Votre produit pourra être rapidement mis sur le marché
+										</p>
+									</>
+								)}
+							</Transition>
+						</div>
+					</div>
 				</div>
 			</WhiteGrid>
 		</div>
