5317da35f3dc403efc22dc09344398506c0713dd
Antoine Brique <antoine.brique@gmail.com>
2024-04-22 17:31:14 +0200

    :dizzy: Update grid-transition animation : squares disapear speeder when you scroll to the bottom

--- src/_components/blocks/grid-transition.tsx
+++ src/_components/blocks/grid-transition.tsx
@@ -32,7 +32,7 @@ const GridTransition = () => {
 				scrollTrigger: {
 					trigger: gridRef.current,
 					start: isMobile ? "top 250%" : "top 200%", 
-					end: isMobile ?  "bottom 60%" : "bottom 70%",
+					end: isMobile ?  "bottom 150%" : "bottom 90%",
 					scrub: true,
 					// markers: true
 				}
