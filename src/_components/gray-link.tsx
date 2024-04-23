4714d8ce5b1d9b084947e909487cc96cbc63e9c0
Antoine Brique <antoine.brique@gmail.com>
2024-04-23 16:19:37 +0200

    :dizzy: Add hover animation for 'découvrir le projet' - gray-link

--- src/_components/gray-link.tsx
+++ src/_components/gray-link.tsx
@@ -7,11 +7,11 @@ type Props = {
 }
 
 const GrayLink = ({ title, isWhite = false, type = "next" }: Props) => {
-	return <div className="group">
-		<button className={`pr-2 pb-2 flex gap-2 justify-center items-center border-b ${isWhite ? "border-b-zinc-50" : "border-b-zinc-500"} group-hover:!border-primary`}>
-			{type === "previous" && <ArrowLeftIcon className={`h-4 w-4 lg:h-6 lg:w-6 ${isWhite ? "text-zinc-50" : "text-zinc-500"} group-hover:!text-primary`} />}
-			<p className={`!text-sm lg:text-inherit ${isWhite ? "!text-zinc-50" : "!text-zinc-500"} group-hover:!text-primary`}>{title}</p>
-			{type === "next" && <ArrowRightIcon className={`h-4 w-4 lg:h-6 lg:w-6 ${isWhite ? "text-zinc-50" : "text-zinc-500"} group-hover:!text-primary`} />}
+	return <div className="gray-link">
+		<button className={`pr-2 pb-2 flex gap-2 justify-center items-center border-b ${isWhite ? "border-b-zinc-50" : "border-b-zinc-500"} `}>
+			{type === "previous" && <ArrowLeftIcon className={`h-4 w-4 lg:h-6 lg:w-6 ${isWhite ? "text-zinc-50" : "text-zinc-500"}`} />}
+			<p className={`!text-sm lg:text-inherit ${isWhite ? "!text-zinc-50" : "!text-zinc-500"}`}>{title}</p>
+			{type === "next" && <ArrowRightIcon className={`h-4 w-4 lg:h-6 lg:w-6 ${isWhite ? "text-zinc-50" : "text-zinc-500"}`} />}
 		</button>
 	</div>;
 };
