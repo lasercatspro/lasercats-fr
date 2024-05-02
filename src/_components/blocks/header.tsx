1c1aab64ae10981972505a65ee339dc074032e95
Antoine Brique <antoine.brique@gmail.com>
2024-05-02 15:05:41 +0200

    :lipstick: Update custom-slider and contact-us button on mobile (herder block)

--- src/_components/blocks/header.tsx
+++ src/_components/blocks/header.tsx
@@ -23,8 +23,11 @@ const Header = (): ReactNode => {
 					<span className='pt-12 text-[2.25rem] md:text-[3.25rem] text-zinc-50 !leading-snug'>Nous fabriquons des applications Web  et mobile aux petits oignons.</span>
 					<div className="mt-24">
 						<p className="mb-8 text-zinc-50 opacity-50">Besoin ponctuel ou projet de long terme ?</p>
-						<Link href={"/contact"}>
-							<SpecialButton title="Nous contacter" />
+						<Link href={"/contact"} className="hidden lg:block">
+							<SpecialButton title="Nous contacter" /> 
+						</Link>
+						<Link href={"/contact"} className="lg:hidden primary h-6 rounded-sm !w-[180px]">
+							Nous contacter
 						</Link>
 					</div>
 				</div>
