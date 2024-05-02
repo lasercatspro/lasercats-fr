5b2e583ccb84fd293c38832f4c3ee4f5557115f6
Antoine Brique <antoine.brique@gmail.com>
2024-05-02 17:37:31 +0200

    :lipstick: Update quote with picture of authors

--- src/_components/quote.tsx
+++ src/_components/quote.tsx
@@ -15,16 +15,32 @@ const Quote = ({ quote }: { quote: QuoteI }) => {
 						width={300}
 						height={100}
 						className="h-[50px] !w-auto"
+						quality={90}
+						priority={false}
+						loading="lazy"
 					/>
 					<p className="text-xs lg:text-sm leading-4 lg:line-clamp-none xl:leading-6 !text-zinc-50 !text-opacity-80">
 						{quote.content}
 					</p>
 				</div>
 				<div className="flex items-center gap-2 ">
-					<i className="h-12 w-12 rounded-full bg-gray-500" />
+					<Image
+						src={quote.imgSrc}
+						height={50}
+						width={50}
+						alt={quote.author}
+						className="rounded-full"
+						quality={90}
+						priority={false}
+						loading="lazy"
+					/>
 					<div>
-						<p className="ml-4 text-zinc-100 text-xs lg:text-base">{quote.author}</p>
-						<p className="ml-4 text-zinc-400 text-xs lg:text-sm">{quote.authorStatus}</p>
+						<p className="ml-2 text-zinc-100 text-xs lg:text-base">
+							{quote.author}
+						</p>
+						<p className="ml-2 text-zinc-400 text-xs lg:text-sm">
+							{quote.authorStatus}
+						</p>
 					</div>
 				</div>
 			</div>
