e33d623c44d7eee58bd85e258505a84928a5e262
Francois Brault <brault.fra@gmail.com>
2024-05-08 16:01:34 +0200

    :lipstick: grid responsiveness

--- src/_components/white-grid.tsx
+++ src/_components/white-grid.tsx
@@ -11,28 +11,26 @@ type Props = {
 
 const WhiteGrid = ({ children, type }: Props) => {
 	const isMobile = useIsMobile({ forIpad: true });
-	const tills = [
-		// Bottom Left "bl"
-		{ coord: `${isMobile ? 5 : 0},7`, place: "bl", color: "green" },
-		{ coord: `${isMobile ? 9 : 2},1`, place: "bl", color: "green" },
-		{ coord: `${isMobile ? 9 : 2},7`, place: "bl", color: "green" },
-		isMobile ? { coord: "3,3", place: "bl", color: "green" } : undefined,
-		isMobile ? { coord: "4,6", place: "bl", color: "green" } : undefined,
-		isMobile ? { coord: "11,5", place: "bl", color: "green" } : undefined,
-		// Bottom Right "br"
-		{ coord: `${isMobile ? 5 : 0},1`, place: "br", color: "blue" },
-		{ coord: `${isMobile ? 5 : 0},5`, place: "br", color: "blue" },
-		isMobile ? { coord: "11,3", place: "br", color: "blue" } : undefined,
-		// Top Right "tr"
-		{ coord: `${isMobile ? 10 : 3},6`, place: "tr", color: "green" },
-		isMobile ? { coord: "4,2", place: "tr", color: "green" } : undefined,
-		// Top Left "tl"
-		{ coord: `${isMobile ? 7 : 1},2`, place: "tl", color: "green" },
-		{ coord: `${isMobile ? 10 : 3},2`, place: "tl", color: "blue" },
+
+	const desktop = [
+		["nul", "gbr", "nul", "nul", "nul", "bbl", "nul"],
+		["nul", "nul", "nul", "nul", "nul", "nul", "nul"],
+		["nul", "nul", "nul", "nul", "nul", "nul", "gbl"],
+		["nul", "btr", "nul", "nul", "nul", "gtr", "nul"],
 	];
 
-	const rows = isMobile ? 15 : 4;
-	const cols = 10;
+	const mobile = [
+		["nul", "nul", "nul", "nul"],
+		["bbr", "nul", "gbr", "nul"],
+		["nul", "nul", "nul", "gtl"],
+		["nul", "nul", "nul", "nul"],
+		["nul", "nul", "nul", "nul"],
+		["nul", "nul", "nul", "bbr"],
+		["gtr", "nul", "nul", "nul"],
+	];
+	const array = isMobile ? mobile : desktop;
+	const rows = array.length;
+	const cols = array[0].length;
 
 	return (
 		<div className="sticky bg-transparent top-0 left-0 h-screen overflow-x-hidden">
@@ -41,46 +39,46 @@ const WhiteGrid = ({ children, type }: Props) => {
 			</div>
 			{/* Grid Structure */}
 			<div
-				style={{ zIndex: -100 }}
-				className="absolute top-0 -left-4 lg:-left-10 bg-transparent w-[120vw] grid grid-cols-10 h-screen"
+				style={{ zIndex: -100, gridTemplateColumns: `repeat(${cols}, 1fr)` }}
+				className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-transparent w-full h-full xl:h-5/6 xl:w-5/6 grid grid-cols-10 "
+
 			>
-				{Array.from(Array(rows)).map((_row, rowIndex) =>
-					Array.from(Array(cols).keys()).map((col, colIndex) => {
-						const till = tills
-							.filter((i) => i?.coord === `${rowIndex},${colIndex}`)
-							?.at(0);
-						const isGreen = till?.color === "green";
-						return (
-							<div
-								key={col}
-								style={{
-									background: isGreen
-										? `linear-gradient(${(till?.place === "bl" && "220deg") ||
-										(till?.place === "br" && "135deg") ||
-										(till?.place === "tl" && "315deg") ||
-										(till?.place === "tr" && "45deg")
-										}, rgba(255,255,255,0) 0%, rgba(0,198,94,0.02) 62%, rgba(0,198,94,0.3351541300113795) 100%)`
-										: !isGreen
-											? `linear-gradient(${(till?.place === "bl" && "220deg") ||
-											(till?.place === "br" && "135deg") ||
-											(till?.place === "tl" && "315deg") ||
-											(till?.place === "tr" && "45deg")
-											}, rgba(255,255,255,0) 0%, rgba(6,3,141,0.015) 62%, rgba(6,3,141,0.3351541300113795) 100%)`
-											: "",
-								}}
-								className={`transition-opacity duration-500 ease-in
-									${till?.color ? "" : "border border-gray-200 border-opacity-20"}
-									${isGreen || !till?.color
-								? "opacity-100"
-								: (type === "skill" || type === "vite") && !isGreen
-									? "opacity-100"
-									: "opacity-0"
-							}`}
-							// aspect-square if you want perfect squares
-							/>
-						);
-					})
+
+				{array.map(
+					(row, rowIndex) => row.map(
+						(col, colIndex): ReactNode => {
+							const color = col.slice(0, 1);
+							const active = color === "g" || color === "b" && ["skill", "vite"].includes(type);
+							let gradient = "";
+							if (color === "g") {
+								gradient = "rgba(255,255,255,0) 0%, rgba(0,198,94,0.02) 62%, rgba(0,198,94,0.33) 100%";
+							} else if (col.slice(0, 1) === "b") {
+								gradient = "rgba(255,255,255,0) 0%, rgba(6,3,141,0.015) 62%, rgba(6,3,141,0.33) 100%";
+							}
+							let orientation = "";
+							switch (col.slice(1, 3)) {
+							case "bl":
+								orientation = "220deg"; break;
+							case "br":
+								orientation = "135deg"; break;
+							case "tl":
+								orientation = "315deg"; break;
+							case "tr":
+								orientation = "45deg"; break;
+							}
+							console.log(col, orientation, color);
+
+							return <div
+								key={`${rowIndex}-${colIndex}`}
+								data-orientation={orientation}
+								data-color={color}
+								id={`${rows}/${rowIndex} ${col}`}
+								style={orientation && color ? { background: `linear-gradient(${orientation}, ${gradient})`, opacity: active ? 1 : 0 } : {}}
+								className="transition-opacity delay-500 duration-500 ease-in bg-opacity-0 "
+							/>;
+						})
 				)}
+
 			</div>
 		</div>
 	);
